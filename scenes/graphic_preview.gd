29a521238c5f0fc2078124c75f73342a6a270a15
jr-dragon <j.dragon@chivincent.net>
2025-08-17 02:11:30 +0800

    Feat: Graphic Preview Scaling
    Squashed commit of the following:
    
    commit 6572a1f2323cadc0dd7ee9746549727dc49d0b69
    Author: jr-dragon <j.dragon@chivincent.net>
    Date:   Sun Aug 17 02:08:11 2025 +0800
    
        feat: scale up and down

--- scenes/graphic_preview.gd
+++ scenes/graphic_preview.gd
@@ -1,5 +1,7 @@
 extends TextureRect
 
+@onready var parent = $".."
+
 var graphic_info: GraphicInfoData:
 	set(val):
 		graphic_info = val
@@ -13,6 +15,13 @@ var palette_data: PackedByteArray:
 		palette_data = val
 		_render()
 
+var img_scales: Array = []
+var img_scales_index: int = 0
+
+func _ready() -> void:
+	gui_input.connect(_on_gui_input)
+
+
 func _on_gamer_version_chooser_game_file_opened(_info: PackedByteArray, data_file: FileAccess) -> void:
 	graphic_data_file = data_file
 
@@ -24,6 +33,28 @@ func _on_graphic_info_previewer_current_updated(data: GraphicInfoData) -> void:
 func _on_left_v_box_container_palette_file_opened(palette: PackedByteArray) -> void:
 	palette_data = palette
 
+
+func _on_gui_input(event: InputEvent) -> void:
+	if texture == null:
+		return
+	
+	var scale_up: bool = Input.is_action_just_pressed("scale_up")
+	var scale_down: bool = Input.is_action_just_pressed("scale_down")
+	
+	if scale_up: img_scales_index += 1
+	if scale_down && img_scales_index > 0: img_scales_index -= 1
+	
+	if scale_up || scale_down:
+		var img = texture.get_image()
+		var max_size = parent.size
+		if img.get_width() * 1.1 > max_size[0] || img.get_height() * 1.1 > max_size[1]:
+			img_scales_index -= 1
+		
+		if img_scales_index == len(img_scales):
+			img.resize(img.get_width() * 1.1, img.get_height() * 1.1)
+			img_scales.push_back(img)
+
+		texture.set_image(img_scales[img_scales_index])
 
 func _render() -> void:
 	if graphic_info == null || graphic_data_file == null || palette_data.is_empty():
@@ -43,4 +74,15 @@ func _render() -> void:
 		if graphic.Data[i] >= len(palette):
 			printerr("[Graphic Data] Palette Data out of range: %d", graphic.Data[i])
 		img.set_pixel(i%graphic_info.Width, graphic_info.Height-i/graphic_info.Width, palette[graphic.Data[i]])
-	texture = ImageTexture.create_from_image(img)
+
+	img_scales.clear()
+	img_scales.push_back(img)
+	img_scales_index = 0
+
+	if texture == null:
+		texture = ImageTexture.create_from_image(img)
+	else:
+		if img.get_size() == texture.get_image().get_size():
+			texture.update(img)
+		else:
+			texture.set_image(img)
